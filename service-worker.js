53560d84f35a44831edd38467de363da738674f4
jhasmine16 <jhca.ombid.ui@phinmaed.com>
2026-03-21 08:03:07 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("Patient Sugar Checke").then(function(cache){
-      return cache.addAll(["index.html"])
-    })
-  );
-});
